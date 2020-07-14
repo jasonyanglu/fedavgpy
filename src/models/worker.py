30f4368ba35d80fe225998fba69ec00d02a6f2cf
Yang Lu <lylylytc@gmail.com>
2020-07-14 16:50:56 +0800

    Update worker.py

--- src/models/worker.py
+++ src/models/worker.py
@@ -83,26 +83,24 @@ class Worker(object):
         pred_total = []
 
         train_loss = 0
-        # for epoch in range(self.num_epoch):
-            # for batch_idx, (x, y) in enumerate(train_dataloader):
-        for i in range(self.num_epoch * 10):
-            x, y = next(iter(train_dataloader))
-            if self.gpu:
-                x, y = x.cuda(), y.cuda()
+        for epoch in range(self.num_epoch):
+            for batch_idx, (x, y) in enumerate(train_dataloader):
+                if self.gpu:
+                    x, y = x.cuda(), y.cuda()
 
-            self.optimizer.zero_grad()
-            pred = self.model(x)
+                self.optimizer.zero_grad()
+                pred = self.model(x)
 
-            loss = criterion(pred, y)
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(self.model.parameters(), 60)
-            self.optimizer.step()
+                loss = criterion(pred, y)
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), 60)
+                self.optimizer.step()
 
-            _, predicted = torch.max(pred, 1)
-            train_loss += loss.item() * y.size(0)
+                _, predicted = torch.max(pred, 1)
+                train_loss += loss.item() * y.size(0)
 
-            pred_total.extend(predicted.cpu().numpy())
-            y_total.extend(y.cpu().numpy())
+                pred_total.extend(predicted.cpu().numpy())
+                y_total.extend(y.cpu().numpy())
 
         train_total = len(y_total)
 
